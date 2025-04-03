8a790fbe516fb234ada11d04afb0c8c9427e6e48
David González Córdoba <dbgc977@gmail.com>
2025-04-02 22:38:25 -0600

    Implement of User class

--- Project1/Usuario.cpp
+++ Project1/Usuario.cpp
@@ -1,1 +1,42 @@
 #include "Usuario.h"
+
+Usuario::Usuario(std::string cedula, std::string nombre, std::string apellido, bool estado)
+{
+	this->cedula = cedula;
+	this->nombre = nombre;
+	this->apellido = apellido;
+	this->estado = estado;
+}
+
+Usuario::~Usuario()
+{
+	// Destructor
+}
+
+std::string Usuario::getCedula() const
+{
+	return cedula;
+}
+
+bool Usuario::getEstado() const
+{
+	return estado;
+}
+
+void Usuario::setEstado(bool estado)
+{
+	this->estado = estado;
+}
+
+std::string Usuario::toString() const
+{
+	std::ostringstream oss;
+	oss << "====================" << std::endl;
+	oss << "Usuario: " << std::endl;
+	oss << "Cedula: " << cedula << std::endl;
+	oss << "Nombre: " << nombre << std::endl;
+	oss << "Apellido: " << apellido << std::endl;
+	oss << "Estado: " << (estado ? "Activo" : "Inactivo") << std::endl;
+	oss << "====================" << std::endl;
+	return oss.str();
+}
--- Project1/Usuario.h
+++ Project1/Usuario.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <iostream>
+#include <sstream>
+
 class Usuario
 {
+private:
+	std::string cedula;
+	std::string nombre;
+	std::string apellido;
+	bool estado;
+public:
+	Usuario(std::string cedula, std::string nombre, std::string apellido, bool estado);
+	~Usuario();
+	std::string getCedula() const;
+	bool getEstado() const;
+	void setEstado(bool estado);
+	std::string toString() const;
+
+
+	
+
 };
 
